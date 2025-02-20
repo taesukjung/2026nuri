24512d70e692c652784e4a3c87c348e75e9fef11
A11486 <taesuk@nis.co.kr>
2025-02-20 15:45:50 +0900

    영문페이지 업데이트

--- public/js/common.js
+++ public/js/common.js
@@ -314,3 +314,20 @@ $(function(){
 	});
 });
 
+//layout html 불러오기
+window.addEventListener('load', function() {
+            var allElements = document.getElementsByTagName('*');
+            Array.prototype.forEach.call(allElements, function(el) {
+                var includePath = el.dataset.includePath;
+                if (includePath) {
+                    var xhttp = new XMLHttpRequest();
+                    xhttp.onreadystatechange = function () {
+                        if (this.readyState == 4 && this.status == 200) {
+                            el.outerHTML = this.responseText;
+                        }
+                    };
+                    xhttp.open('GET', includePath, true);
+                    xhttp.send();
+                }
+            });
+        });
